ee4763641225c9845ffc54bc70854340f208624f
zzl5357 <zzl5357@icloud.com>
2020-12-04 14:54:23 -0800

    0.30

--- myblockmm.c
+++ myblockmm.c
@@ -75,7 +75,7 @@ void *mythreaded_vector_blockmm(void *t)
                     vc2 = _mm256_load_pd(&c[ii+1][jj]);
                     vc3 = _mm256_load_pd(&c[ii+1][jj+1]);
 
-                for(kk = k; kk < k+(ARRAY_SIZE/n); kk++)
+                for(kk = k; kk < k+(ARRAY_SIZE/n)-1; kk++)
                 {
                         va = _mm256_broadcast_sd(&a[ii][kk]);
                         vb = _mm256_load_pd(&b[kk][jj]);
