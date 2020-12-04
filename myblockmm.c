d201a3334507a3b90deb37b4342fbb86bd24f78c
zzl5357 <zzl5357@icloud.com>
2020-12-03 23:45:20 -0800

    0.10

--- myblockmm.c
+++ myblockmm.c
@@ -67,7 +67,7 @@ void *mythreaded_vector_blockmm(void *t)
   int tile_size = ARRAY_SIZE/n;
 
 #pragma omp parallel shared(a,b,c) private(i,j,k)
-{ 
+{
   # pragma omp for
   for(i = (block_size)*(tid); i < (block_size)*(tid+1); i+=tile_size)
   {
@@ -75,9 +75,9 @@ void *mythreaded_vector_blockmm(void *t)
     {
       for(k = 0; k < ARRAY_SIZE; k+=(tile_size))
       {
-        _mm_prefetch(&a, _MM_HINT_T0);
-        _mm_prefetch(&b, _MM_HINT_T0);
-        _mm_prefetch(&c, _MM_HINT_T0);
+        _mm_prefetch(&a[i][k], _MM_HINT_T0);
+        _mm_prefetch(&b[k][j], _MM_HINT_T0);
+        _mm_prefetch(&c[i][j], _MM_HINT_T0);
          for(ii = i; ii < i+(tile_size); ii++)
          {
             for(jj = j; jj < j+(tile_size); jj+=VECTOR_WIDTH)
